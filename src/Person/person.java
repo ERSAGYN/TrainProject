e3eee744068fc177d769f6bd019ae889314ce00c
Пользователь <diaskudaibergen2016@gmail.com>
2023-01-18 19:07:55 +0600

    class person

---
+++ src/Person/person.java
@@ -0,0 +1,30 @@
+package Person;
+
+public class person {
+    private String name;
+    private int age;
+    private boolean disability;
+    private int id;
+
+    public person(String name, int age, boolean disability, int id){
+        this.age = age;
+        this.disability = disability;
+        this.id = id;
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean getDisability(){
+        return disability;
+    }
+}
